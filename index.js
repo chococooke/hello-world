7c55e8124d59f67b4849092fb2b9ed61b0ae1699
chococooke * web3 <pro.bhimsen@gmail.com>
2025-05-19 11:45:16 +0530

    add basic features

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 module.exports.helloWorld = () => "Hello world";
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(3000, () => console.log("server is running on port 3000"));
