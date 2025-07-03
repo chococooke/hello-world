e5c8e68fa083302acd6706a1c0116809419bbee4
chococooke * web3 <pro.bhimsen@gmail.com>
2025-07-03 08:28:29 +0530

    update env

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ module.exports.helloWorld = () => "Hello world";
 
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config({quiet: true});
+
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors());
@@ -10,4 +15,4 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(3000, () => console.log("server is running on port 3000"));
+app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
